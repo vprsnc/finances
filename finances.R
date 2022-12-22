5e28b9126d67d1c54067e9c03ade4a477da144db
Georgy <heavypresence@riesup.net>
2022-12-23 01:26:36 +0300

    func

--- finances.R
+++ finances.R
@@ -1,21 +1,17 @@
 library(DBI)
 library(dbplyr)
-
 personaldb <- dbConnect(duckdb::duckdb(), dbdir="/home/georgy/personal.duckdb", read_only=F)
-
-categories <- c("Smoking", "Booze", "Transport", "Eatout", "Products", "Bigbuy",
-                "Rent", "Taxes", "Salary", "Earnings")
-
 transaction <- function(tdate, ttime, amount, category, commentary){
   values <- list(Transaction.Date=tdate, Transaction.Time=ttime,
                  Amount=amount, Category=category, Commentary=commentary)
   data.frame(values)
 }
-
 Transaction <- transaction(
   tdate = readline("Date in format YYYY-mm-DD: "),
   ttime = readline("Time in format HH:MM: "),
   amount = readline("Amount, negative '-' for spending: "),
-  category = readline(categories),
+  category = readline("Category: "),
   commentary = readline("what did you do: ")
 )
+dbAppendTable(personaldb, "finances", Transaction)
+dbDisconnect(personaldb, shutdown=T)
